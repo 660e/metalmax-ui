7f772b8a5ebd0971cad7e771dfc913f9f02cc29b
Min <solunar@vip.qq.com>
2024-08-01 20:37:43 +0800

    Update

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,5 +3,8 @@ defineOptions({ name: 'app-home' });
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="h-screen bg-slate-500 flex flex-col justify-center items-center">
+    <div class="text-3xl">重装机兵</div>
+    <div class="text-xl">Metal Max</div>
+  </div>
 </template>
