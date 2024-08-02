18a23fc8297fd5fe192d6bad1f816f3ece921b46
Min <solunar@vip.qq.com>
2024-08-02 23:19:33 +0800

    Update

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -4,11 +4,15 @@ defineProps<{ menus: { label: string; value: string }[] }>();
 </script>
 
 <template>
-  <div class="h-full flex">
-    <div class="w-60 bg-red-500/50">
-      <div>
-        <div v-for="menu in menus" :key="menu.value">{{ menu.label }}</div>
+  <div class="h-full text-white flex">
+    <div class="w-60 flex flex-col">
+      <div class="h-12">场景信息</div>
+      <div class="flex-1 space-y-2">
+        <div v-for="menu in menus" :key="menu.value" class="px-4 py-2 bg-black/50 backdrop-blur w-36 duration-200 hover:w-40 hover:pl-8">
+          <span>{{ menu.label }}</span>
+        </div>
       </div>
+      <div class="h-12">金钱</div>
     </div>
 
     <div class="flex-1 bg-green-500/50">
