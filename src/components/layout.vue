d0f2f589d28387322c5bf842059a996641dc5096
Min <solunar@vip.qq.com>
2024-08-11 22:04:00 +0800

    Update

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,38 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
 defineOptions({ name: 'q-layout' });
 defineProps<{ menus: { label: string; value: string }[] }>();
-defineEmits<{ handle: [value: string] }>();
 
-const active = ref();
+const active = defineModel<string>();
 </script>
 
 <template>
   <div class="h-full text-gray-200 flex relative">
-    <div class="absolute z-20 top-0 left-0 w-[500px] h-12 flex flex-col">
-      <div class="flex-1 flex items-center bg-gradient-to-r from-neutral-900 from-35%">
-        <span class="pl-4 text-lg leading-none">场景信息</span>
+    <transition name="slide-top">
+      <div v-if="active" class="absolute z-20 top-0 left-0 w-[500px] h-12 flex flex-col">
+        <div class="flex-1 flex items-center bg-gradient-to-r from-neutral-900 from-40%">
+          <span class="pl-4 text-xl leading-none">场景信息</span>
+        </div>
+        <b class="h-px bg-gradient-to-r from-white/30 to-white/0"></b>
       </div>
-      <b class="h-px bg-gradient-to-r from-white/30 to-white/0"></b>
-    </div>
+    </transition>
 
-    <div class="w-40 pt-24 flex flex-col">
-      <div class="flex-1 space-y-2">
-        <div
-          v-for="menu in menus"
-          :key="menu.value"
-          :class="{ 'active w-32 pl-8': active === menu.value }"
-          @click="
-            $emit('handle', menu.value);
-            active = menu.value;
-          "
-          class="backdrop handle px-4 py-2 w-28 border border-l-0 border-white/30 duration-200 hover:w-32 hover:pl-8"
-        >
-          {{ menu.label }}
+    <transition name="slide-left">
+      <div v-if="active" class="w-40 pt-24 flex flex-col">
+        <div class="flex-1 space-y-2">
+          <div
+            v-for="menu in menus"
+            :key="menu.value"
+            :class="{ 'active w-32 pl-8': active === menu.value }"
+            @click="active = menu.value"
+            class="backdrop handle px-4 py-2 w-28 border border-l-0 border-white/30 duration-200 hover:w-32 hover:pl-8"
+          >
+            {{ menu.label }}
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
 
     <div class="flex-1">
       <slot />
--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 import QLayout from '@/components/layout.vue';
 import QTeam from '@/components/team.vue';
 
 defineOptions({ name: 'app-menu' });
 
-const active = ref('items');
+const active = ref();
 const menus = [
   { label: '地图', value: 'map' },
   { label: '乘降', value: 'in-out' },
@@ -16,15 +16,22 @@ const menus = [
   { label: '车库', value: 'garage' },
   { label: '选项', value: 'settings' }
 ];
+
+onMounted(() => (active.value = 'menu'));
 </script>
 
 <template>
-  <q-layout :menus="menus" @handle="value => (active = value)">
-    <div v-if="active === 'items'" class="backdrop h-full border-x border-white/30 flex"></div>
+  <q-layout v-model="active" :menus="menus">
+    <transition name="slide-right">
+      <div v-if="active === 'items'" class="backdrop h-full border-r border-white/30 flex">
+        <div class="backdrop w-10 border-x border-white/30"></div>
+        <div class="flex-1"></div>
+      </div>
+    </transition>
 
     <template #aside>
-      <transition name="slide">
-        <q-team v-if="[undefined, 'items'].includes(active)" />
+      <transition name="slide-right">
+        <q-team v-if="['menu', 'items'].includes(active)" />
       </transition>
     </template>
   </q-layout>
