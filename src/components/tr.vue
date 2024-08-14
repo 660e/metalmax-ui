7cd4f93932d5935d8a61176c59f1e162d6376990
Min <solunar@vip.qq.com>
2024-08-14 22:17:09 +0800

    Update

--- src/components/tr.vue
+++ src/components/tr.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineOptions({ name: 'q-tr' });
-defineProps<{ cells: string[]; widths: number[] }>();
+defineProps<{ cells: string[]; widths?: number[] }>();
 </script>
 
 <template>
@@ -8,8 +8,8 @@ defineProps<{ cells: string[]; widths: number[] }>();
     <span
       v-for="(cell, index) in cells"
       :key="index"
-      :style="{ width: widths[index] ? `${widths[index]}px` : 'auto' }"
-      :class="{ 'flex-1': widths[index] === 0, 'justify-end': index }"
+      :style="{ width: widths && widths[index] ? `${widths[index]}px` : 'auto' }"
+      :class="{ 'flex-1': !widths || !widths[index], 'justify-end': index }"
       class="flex items-center px-4"
     >
       {{ cell }}
