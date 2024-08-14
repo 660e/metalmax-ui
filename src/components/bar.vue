a0b0791a8de012acf949d1162e8c1f5133209cd3
Min <solunar@vip.qq.com>
2024-08-14 21:05:21 +0800

    Update

--- src/components/bar.vue
+++ src/components/bar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineOptions({ name: 'q-bar' });
-withDefaults(defineProps<{ label: string; current: number; max: number; color: string }>(), { current: 1, max: 1 });
+defineProps<{ label: string; current: number; color: string }>();
 </script>
 
 <template>
@@ -10,12 +10,12 @@ withDefaults(defineProps<{ label: string; current: number; max: number; color: s
       <span class="flex-1"></span>
       <span class="text-lg leading-none">{{ current }}</span>
       <span class="text-gray-300">/</span>
-      <span class="text-gray-300">{{ max }}</span>
+      <span class="text-gray-300">9999</span>
     </div>
     <div class="flex items-center h-4 -mt-0.5">
       <span class="w-px h-full bg-gradient-to-b from-white/0 via-white/30 to-white/0"></span>
       <span class="flex-1 h-2 border-y border-neutral-900/30 flex">
-        <b :style="{ width: `${(current / max) * 100}%` }" :class="[color]"></b>
+        <b :style="{ width: `${(current / 9999) * 100}%` }" :class="[color]"></b>
       </span>
       <span class="w-px h-full bg-gradient-to-b from-white/0 via-white/30 to-white/0"></span>
     </div>
--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
-import items from '@/assets/data/items.json';
+import { onMounted, ref } from 'vue';
 
 defineOptions({ name: 'app-menu' });
 
@@ -14,18 +13,6 @@ const menus = [
   { label: '车库', value: 'garage' },
   { label: '选项', value: 'settings' }
 ];
-const group = ref([1]);
-const groupName = computed(() => {
-  if (group.value[0] === 1) {
-    return '道具';
-  } else if (group.value[0] === 2) {
-    return '贵重道具';
-  } else if (group.value[0] > 10 && group.value[0] < 20) {
-    return '装备';
-  } else {
-    return '-';
-  }
-});
 
 onMounted(() => (active.value = 'items'));
 </script>
@@ -38,16 +25,16 @@ onMounted(() => (active.value = 'items'));
           <div @click="group = [1]" class="w-8 h-8 bg-red-500"></div>
           <div @click="group = [2]" class="w-8 h-8 bg-red-500"></div>
           <div @click="group = [11]" class="w-8 h-8 bg-red-500"></div>
-          <img src="@/assets/images/v1.png" />
-          <img src="@/assets/images/v2.png" />
-          <img src="@/assets/images/v3.png" />
-          <img src="@/assets/images/v4.png" />
+          <img src="@/assets/t1.png" />
+          <img src="@/assets/t2.png" />
+          <img src="@/assets/t3.png" />
+          <img src="@/assets/t4.png" />
         </div>
         <div class="backdrop-shallow flex-1 pt-12">
           <div class="h-6"></div>
-          <div class="h-6 pl-4 flex items-center">{{ groupName }}</div>
+          <div class="h-6 pl-4 flex items-center">---</div>
           <q-thead :labels="['名称', '持有数']" :widths="[0, 100]" />
-          <q-tr v-for="item in items.filter(item => group.includes(item.type))" :key="item.name" :cells="[item.name, 1]" :widths="[0, 100]" />
+          <!-- <q-tr v-for="item in items.filter(item => group.includes(item.type))" :key="item.name" :cells="[item.name, 1]" :widths="[0, 100]" /> -->
         </div>
       </div>
     </transition>
