8c065d81c8bb3298d83c5573de9e6c809143e569
Min <solunar@vip.qq.com>
2024-08-16 21:22:44 +0800

    Update

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -10,13 +10,35 @@ const active = defineModel<string>();
     <transition name="slide-top">
       <div v-if="active" class="absolute z-20 top-0 left-0 w-[500px] h-12 flex flex-col">
         <div class="flex-1 flex items-center pl-4 bg-gradient-to-r from-neutral-900 from-40%">
-          <span class="text-xl leading-none">场景信息</span>
+          <span class="text-xl leading-none">{{ menus.find(menu => menu.value === active)?.label || '场景信息' }}</span>
         </div>
         <b class="h-px bg-gradient-to-r from-white/30"></b>
       </div>
     </transition>
 
-    <div class="flex-1"></div>
+    <div class="flex-1 flex">
+      <transition name="slide-left">
+        <div v-if="active" class="w-40 pt-24 space-y-1">
+          <div
+            v-for="menu in menus"
+            :key="menu.value"
+            :class="[active === menu.value ? 'w-32 pl-8' : 'w-28 pl-4']"
+            @click="active = menu.value"
+            class="panel-1 py-2 border border-white/30 border-l-0 duration-200 hover:w-32 hover:pl-8"
+          >
+            {{ menu.label }}
+          </div>
+        </div>
+      </transition>
+
+      <div class="flex-1 flex">
+        <slot />
+      </div>
+
+      <div class="w-80 flex">
+        <slot name="aside" />
+      </div>
+    </div>
 
     <transition name="slide-bottom">
       <div v-if="active" class="panel-2 h-12 border-t border-white/30"></div>
--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -9,9 +9,9 @@ const active = ref();
 const menus = [
   { label: '地图', value: 'map' },
   { label: '任务', value: 'quests' },
-  { label: '乘降', value: 'in-out' },
   { label: '道具', value: 'items' },
   { label: '装备', value: 'equip' },
+  { label: '乘降', value: 'in-out' },
   { label: '状态', value: 'status' },
   { label: '车库', value: 'garage' },
   { label: '选项', value: 'settings' }
