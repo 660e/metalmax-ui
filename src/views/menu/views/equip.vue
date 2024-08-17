996416af411290de1d49df7869412f11bdde2364
Min <solunar@vip.qq.com>
2024-08-17 22:22:53 +0800

    Update

--- src/views/menu/views/equip.vue
+++ src/views/menu/views/equip.vue
@@ -6,7 +6,7 @@ defineOptions({ name: 'menu-equip' });
 
 <template>
   <div class="flex-1 flex">
-    <div class="panel-2 border-x border-white/30 pt-24 px-1">
+    <div class="panel-2 border-x border-white/30 pt-24 px-1 space-y-1">
       <div v-for="item in 8" :key="item" class="w-8 h-8 bg-red-500"></div>
     </div>
     <div class="panel-1 flex-1 flex flex-col border-r border-white/30 pt-12">
