78baf64f0ac2ed50644d73de10eae7fa2b0de686
Min <solunar@vip.qq.com>
2024-08-18 18:31:47 +0800

    Update

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
+import MenuMap from './views/map.vue';
 import MenuItems from './views/items.vue';
 import MenuEquip from './views/equip.vue';
 import MenuStatus from './views/status.vue';
@@ -13,22 +14,22 @@ defineOptions({ name: 'app-menu' });
 const active = ref();
 const menus = [
   { label: '地图', value: 'map' },
-  { label: '任务', value: 'quests' },
+  { label: '乘降', value: 'in-out' },
   { label: '道具', value: 'items' },
   { label: '装备', value: 'equip' },
-  { label: '乘降', value: 'in-out' },
   { label: '状态', value: 'status' },
   { label: '车库', value: 'garage' },
   { label: '选项', value: 'settings' }
 ];
 
-onMounted(() => (active.value = 'menu'));
+onMounted(() => (active.value = 'map'));
 </script>
 
 <template>
   <q-layout v-model="active" :menus="menus">
     <transition name="slide-right">
-      <menu-items v-if="active === 'items'" />
+      <menu-map v-if="active === 'map'" />
+      <menu-items v-else-if="active === 'items'" />
       <menu-equip v-else-if="active === 'equip'" />
       <menu-status v-else-if="active === 'status'" />
       <menu-garage v-else-if="active === 'garage'" />
---
+++ src/views/menu/views/map.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineOptions({ name: 'menu-map' });
+</script>
+
+<template>
+  <div class="absolute top-0 left-40 w-[944px] h-full pb-12 flex">
+    <div class="panel-2 w-10 border-x border-white/30 pt-24 px-px space-y-px">
+      <div v-for="item in 2" :key="item" class="w-9 h-9 bg-red-500"></div>
+    </div>
+    <div class="panel-1 w-[600px] border-r border-white/30"></div>
+    <div class="flex-1"></div>
+    <div class="panel-1 w-[300px] border-l border-white/30"></div>
+  </div>
+</template>
