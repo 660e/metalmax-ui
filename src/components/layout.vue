980032016a120610dd4bf83fbdb4c45c9387811e
Min <solunar@vip.qq.com>
2024-08-21 18:56:39 +0800

    Update

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -8,24 +8,25 @@ const active = defineModel();
 <template>
   <div class="h-full relative">
     <transition name="slide-top">
-      <div v-if="active" @click="active = 'menu'" class="absolute z-20 top-0 left-0 w-[500px] h-12 flex flex-col">
-        <div class="flex-1 flex items-center pl-4 bg-gradient-to-r from-neutral-900 from-40%">
-          <span class="text-xl leading-none">
-            {{ menus.find(menu => menu.value === active)?.label || '场景信息' }}
-          </span>
+      <div
+        v-if="active"
+        @click="active = 'menu'"
+        class="absolute z-20 top-0 left-0 w-[500px] h-12 flex items-center bg-gradient-to-r from-40% from-neutral-900"
+      >
+        <div class="pl-4 text-xl leading-none">
+          {{ menus.find(menu => menu.value === active)?.label || '场景信息' }}
         </div>
-        <b class="h-px bg-gradient-to-r from-white/30"></b>
       </div>
     </transition>
 
     <transition name="slide-left">
-      <div v-if="active" class="absolute z-10 top-24 left-0 w-32 space-y-1">
+      <div v-if="active" class="absolute top-24 left-0 w-32 space-y-1">
         <div
           v-for="menu in menus"
           :key="menu.value"
           :class="[active === menu.value ? 'w-32 pl-8' : 'w-28 pl-4']"
           @click="active = menu.value"
-          class="panel-1 py-2 border border-white/30 border-l-0 duration-200 hover:w-32 hover:pl-8"
+          class="backdrop-30 py-2 duration-200 hover:w-32 hover:pl-8"
         >
           {{ menu.label }}
         </div>
@@ -35,7 +36,7 @@ const active = defineModel();
     <slot />
 
     <transition name="slide-bottom">
-      <div v-if="active" class="absolute bottom-0 left-0 panel-2 w-full h-12 border-t border-white/30"></div>
+      <div v-if="active" class="absolute bottom-0 left-0 backdrop-90 w-full h-12"></div>
     </transition>
   </div>
 </template>
