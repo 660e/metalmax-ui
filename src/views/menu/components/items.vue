4ca3bd219402f168dde561b66b034b7a8e28620b
Min <solunar@vip.qq.com>
2024-08-21 20:12:11 +0800

    Update

--- src/views/menu/components/items.vue
+++ src/views/menu/components/items.vue
@@ -6,6 +6,9 @@
       <q-row>道具</q-row>
       <q-head :cells="['名称', '持有数']" />
       <q-row :cells="['碱性车蜡', 1]" hover />
+      <q-row :cells="['修理工具箱', 1]" hover />
+      <q-row :cells="['回复胶囊', 1]" hover />
+      <q-row :cells="['能量胶囊', 1]" hover />
     </q-panel-600>
   </div>
 </template>
