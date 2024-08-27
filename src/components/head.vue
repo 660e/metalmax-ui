b5086d5751b5fe5c914fd583851189be1d64d177
Min <solunar@vip.qq.com>
2024-08-27 21:09:45 +0800

    Update

--- src/components/head.vue
+++ src/components/head.vue
@@ -4,7 +4,7 @@ defineProps({ cells: Array });
 </script>
 
 <template>
-  <div class="h-6 py-1 flex relative">
+  <div class="h-6 py-1 flex">
     <div
       v-for="(cell, index) in cells"
       :key="index"
@@ -13,6 +13,5 @@ defineProps({ cells: Array });
     >
       <span>{{ cell }}</span>
     </div>
-    <b class="absolute top-1 left-0 w-1 h-4 bg-white/30"></b>
   </div>
 </template>
--- src/views/index.vue
+++ src/views/index.vue
@@ -10,7 +10,7 @@ const routes = [
 
 <template>
   <div class="h-screen font-mono text-sm leading-none select-none relative flex justify-center items-center bg-gray-800 text-neutral-100">
-    <div v-if="!visible" @click="visible = !visible" class="absolute top-2 left-2 p-2 rounded cursor-pointer bg-neutral-600">Menu</div>
+    <div @click="visible = !visible" class="absolute top-2 left-2 p-2 rounded cursor-pointer bg-neutral-600">Menu</div>
     <div :class="[visible ? 'left-0' : '-left-40']" class="absolute top-0 w-40 p-4 space-y-4 rounded-br duration-200 bg-neutral-600">
       <div
         v-for="route in routes"
