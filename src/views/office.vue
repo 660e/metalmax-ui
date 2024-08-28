ee230224335901eb859f3590e6a7a888b87fd39e
Min <solunar@vip.qq.com>
2024-08-28 21:25:21 +0800

    Update

--- src/views/office.vue
+++ src/views/office.vue
@@ -15,23 +15,52 @@ onMounted(() => (active.value = 1));
 <template>
   <q-layout v-model="active" :menus="menus">
     <transition name="slide-right">
-      <div v-if="[1, 2, 3].includes(active)" class="h-full">
+      <div v-if="active === 1" class="h-full">
         <q-panel :size="600" left right>
           <q-row v-for="n in 3" :key="n" />
           <q-row class="pl-4">通缉令</q-row>
           <q-head :cells="['名字', '赏金']" />
-          <q-row :cells="['碱性车蜡', 99]" hover />
-          <q-row :cells="['修理工具箱', 99]" hover />
-          <q-row :cells="['回复胶囊', 99]" hover />
-          <q-row :cells="['能量胶囊', 99]" hover />
+          <q-row :cells="['沙门菌猿一家', 1000]" hover />
+          <q-row :cells="['神风鬼王', 22000]" hover />
+          <q-row :cells="['隆美尔游魂战车', 160000]" hover />
+          <q-row :cells="['帕鲁·巴迪斯', 200000]" hover />
           <q-row v-for="n in 9" :key="n" />
           <q-head :cells="['详细情报']" />
+        </q-panel>
+        <q-panel v-if="active" :size="300" left></q-panel>
+      </div>
+
+      <div v-else-if="active === 2" class="h-full">
+        <q-panel :size="600" left right>
+          <q-row v-for="n in 3" :key="n" />
+          <q-row class="pl-4">领取赏金</q-row>
+          <q-head :cells="['信物', '赏金']" />
+          <q-row :cells="['猴子的指甲泥', 1000]" hover />
+          <q-row :cells="['和平之头带', 22000]" hover />
+          <q-row :cells="['诺亚系统No.R', 160000]" hover />
+          <q-row :cells="['战斗钉鞋', 200000]" hover />
+        </q-panel>
+      </div>
+
+      <div v-else-if="active === 3" class="h-full">
+        <q-panel :size="600" left right>
+          <q-row v-for="n in 3" :key="n" />
+          <q-row class="pl-4">委托任务</q-row>
+          <q-head :cells="['名称']" />
+          <q-row :cells="['任务001']" hover />
+          <q-row :cells="['任务002']" hover />
+          <q-row :cells="['任务003']" hover />
+          <q-row :cells="['任务004']" hover />
+          <q-row v-for="n in 9" :key="n" />
+          <q-head :cells="['详细信息']" />
         </q-panel>
       </div>
     </transition>
 
     <transition name="slide-right">
-      <q-panel v-if="active" :size="300" left></q-panel>
+      <q-panel v-if="['menu', 2, 'exit'].includes(active)" :size="240" left>
+        <q-team />
+      </q-panel>
     </transition>
   </q-layout>
 </template>
