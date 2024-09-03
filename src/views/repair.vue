ebe78d6760791000eecf35d25549b4b492308d08
Min <solunar@vip.qq.com>
2024-09-03 22:35:39 +0800

    Update

--- src/views/repair.vue
+++ src/views/repair.vue
@@ -1,49 +1,49 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 const active = ref();
 const menus = [
   { label: '修理', value: 1 },
   { label: '拖车救援', value: 2 },
-  { label: '离开商店', value: 'exit' }
+  { label: '离开商店', value: 0 }
 ];
-
-onMounted(() => (active.value = 'menu'));
 </script>
 
 <template>
-  <q-layout v-model="active" :menus="menus">
-    <transition name="slide-right">
-      <div v-if="active === 1" class="h-full">
-        <q-panel :size="40" :icons="Array(5)" left />
-        <q-panel :size="600" left right>
-          <q-row v-for="n in 3" :key="n" />
-          <q-row class="pl-4">修理车辆</q-row>
-          <q-head :cells="['部位', '详情', '价格']" />
-          <q-row :cells="['全部修理']" hover />
-          <q-row :cells="['红狼', '破损', 2000]" hover />
-          <q-row :cells="['220mm大地女神炮', '破损', 1000]" hover />
-          <q-row :cells="['烈日中子炮', '大破', 5000]" hover />
-        </q-panel>
-      </div>
-
-      <div v-else-if="active === 2" class="h-full">
-        <q-panel :size="600" left right>
-          <q-row v-for="n in 3" :key="n" />
-          <q-row class="pl-4">选择</q-row>
-          <q-head :cells="['名称']" />
-          <q-row :cells="['战车1']" hover />
-          <q-row :cells="['战车1']" hover />
-          <q-row :cells="['战车1']" hover />
-          <q-row :cells="['战车1']" hover />
-        </q-panel>
-      </div>
-    </transition>
-
-    <transition name="slide-right">
-      <q-panel v-if="active" :size="240" left>
-        <q-team />
+  <q-layout v-model="active" :menus="menus" title="修理店">
+    <template v-if="active === 1">
+      <q-panel :size="41" :icons="Array(5)" lb />
+      <q-panel :size="600" lb rb>
+        <q-tr>修理</q-tr>
+        <q-th :data="['部位', '详情', '价格']" />
+        <div class="flex-1 relative">
+          <q-scroll :range="[0, 19]" />
+          <q-tr :data="['全部修理', 15000]" hover />
+          <q-tr :data="['红狼', '破损', 2000]" hover />
+          <q-tr :data="['220mm大地女神炮', '破损', 2000]" hover />
+          <q-tr :data="['雷暴机关枪', '破损', 2000]" hover />
+          <q-tr :data="['S龙卷风炮', '破损', 2000]" hover />
+        </div>
       </q-panel>
-    </transition>
+    </template>
+
+    <template v-if="active === 2">
+      <q-panel :size="600" lb rb>
+        <q-tr>拖车救援</q-tr>
+        <q-th :data="['名称']" />
+        <q-tr :data="['战车-01']" hover />
+        <q-tr :data="['战车-02']" hover />
+        <q-tr :data="['战车-03']" hover />
+        <q-tr :data="['战车-04']" hover />
+        <q-tr :data="['战车-05']" hover />
+        <q-tr :data="['战车-06']" hover />
+        <q-tr :data="['战车-07']" hover />
+        <q-tr :data="['战车-08']" hover />
+        <q-tr :data="['战车-09']" hover />
+        <q-tr :data="['战车-10']" hover />
+        <q-tr :data="['战车-11']" hover />
+        <q-tr :data="['战车-12']" hover />
+      </q-panel>
+    </template>
   </q-layout>
 </template>
