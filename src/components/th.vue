6632cf8fce9a69f1594b9b4cb1822231be6c988e
Min <solunar@vip.qq.com>
2024-09-04 21:41:50 +0800

    Update

--- src/components/th.vue
+++ src/components/th.vue
@@ -1,10 +1,18 @@
 <script setup>
 defineOptions({ name: 'q-th' });
-defineProps({ data: Array });
+defineProps({ data: Array, width: Array });
 </script>
 
 <template>
   <div class="h-6 py-1 flex text-xs leading-none text-gray-400">
-    <span v-for="(e, i) in data" :key="i" :class="{ 'flex-1 pl-4': !i, 'w-[100px] justify-end ml-px pr-4': i }" class="h-full flex items-center bg-neutral-900/40">{{ e }}</span>
+    <span
+      v-for="(e, i) in data"
+      :key="i"
+      :style="{ width: i ? `${width ? width[i - 1] : 100}px` : '' }"
+      :class="{ 'flex-1 pl-4': !i, 'justify-end ml-px pr-4': i }"
+      class="h-full flex items-center bg-neutral-900/40"
+    >
+      {{ e }}
+    </span>
   </div>
 </template>
--- src/views/supply.vue
+++ src/views/supply.vue
@@ -1,36 +1,28 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 const active = ref();
 const menus = [
   { label: '补给', value: 1 },
-  { label: '离开商店', value: 'exit' }
+  { label: '离开商店', value: 0 }
 ];
-
-onMounted(() => (active.value = 'menu'));
 </script>
 
 <template>
-  <q-layout v-model="active" :menus="menus">
-    <transition name="slide-right">
-      <div v-if="active === 1" class="h-full">
-        <q-panel :size="40" :icons="Array(5)" left />
-        <q-panel :size="600" left right>
-          <q-row v-for="n in 3" :key="n" />
-          <q-row class="pl-4">补给车辆</q-row>
-          <q-head :cells="['部位', '详情', '单价']" />
-          <q-row :cells="['全部补给']" hover />
-          <q-row :cells="['梅卡瓦', '12345/99999', '0.1/片']" hover />
-          <q-row :cells="['220mm大地女神炮', '16/99', '20/枚']" hover />
-          <q-row :cells="['烈日中子炮', '5/16', '500/枚']" hover />
-        </q-panel>
-      </div>
-    </transition>
-
-    <transition name="slide-right">
-      <q-panel v-if="active" :size="240" left>
-        <q-team />
+  <q-layout v-model="active" :menus="menus" title="补给店">
+    <template v-if="active === 1">
+      <q-panel :size="41" :icons="Array(5)" lb />
+      <q-panel :size="600" lb rb>
+        <q-tr>补给</q-tr>
+        <q-th :data="['部位', '详情', '单价']" :width="[150, 100]" />
+        <div class="flex-1 relative">
+          <q-scroll :range="[0, 19]" />
+          <q-tr :data="['全部补给']" hover />
+          <q-tr :data="['梅卡瓦', '12345/99999', '0.1/片']" hover />
+          <q-tr :data="['220mm大地女神炮', '16/99', '20/枚']" hover />
+          <q-tr :data="['S龙卷风炮', '5/16', '1000/枚']" hover number />
+        </div>
       </q-panel>
-    </transition>
+    </template>
   </q-layout>
 </template>
