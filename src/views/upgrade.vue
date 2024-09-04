be29dda0e658f9f32c5f1f49b2aefd2f9eb0e5d9
Min <solunar@vip.qq.com>
2024-09-04 22:46:21 +0800

    Update

--- src/views/upgrade.vue
+++ src/views/upgrade.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 
-const active = ref(1);
+const active = ref(2);
 const menus = [
   { label: '改造底盘', value: 1 },
   { label: '改造武器', value: 2 },
@@ -35,29 +35,31 @@ const menus = [
         <q-tr :data="['费用', 200000]" />
       </q-panel>
     </template>
+
+    <template v-if="active === 2">
+      <q-panel :size="41" :icons="Array(4)" lb />
+      <q-panel :size="600" lb rb>
+        <q-tr>改造武器</q-tr>
+        <q-th :data="['名称', '改造度']" />
+        <div class="flex-1 relative">
+          <q-scroll :range="[0, 12]" />
+          <q-tr :data="['220mm大地女神炮', '75%']" hover />
+          <q-tr :data="['220mm大地女神炮', '75%']" hover />
+          <q-tr :data="['220mm大地女神炮', '75%']" hover />
+          <q-tr :data="['220mm大地女神炮', '75%']" hover />
+        </div>
+        <q-th :data="['改造']" />
+        <q-tr :data="['攻击', 2000]" hover />
+        <q-tr :data="['防御', 255]" hover />
+        <q-tr :data="['弹仓', 99]" hover />
+        <q-th :data="['总结']" />
+        <q-tr :data="['重量', '(+100.00t)100.00t']" down />
+        <q-tr :data="['费用', 200000]" />
+      </q-panel>
+    </template>
+
     <!--
 
-
-      <div v-else-if="active === 2" class="h-full">
-        <q-panel :size="40" :icons="Array(4)" left />
-        <q-panel :size="600" left right>
-          <q-row v-for="n in 3" :key="n" />
-          <q-row class="pl-4">选项</q-row>
-          <q-head :cells="['名称', '改造度']" />
-          <q-row :cells="['220mm大地女神炮', '90%']" />
-          <q-row :cells="['220mm大地女神炮', '90%']" />
-          <q-row :cells="['220mm大地女神炮', '90%']" />
-          <q-row :cells="['220mm大地女神炮', '90%']" />
-          <q-row v-for="n in 8" :key="n" />
-          <q-head :cells="['改造']" />
-          <q-row :cells="['攻击', '(+1000)1000']" hover />
-          <q-row :cells="['守备', '(+1000)1000']" hover />
-          <q-row :cells="['弹仓', '(+1000)1000']" hover />
-          <q-head :cells="['总结']" />
-          <q-row :cells="['重量', '(-99.00t)190.00t']" />
-          <q-row :cells="['费用', 10000000]" />
-        </q-panel>
-      </div>
 
       <div v-else-if="active === 4" class="h-full">
         <q-panel :size="40" :icons="Array(4)" left />
