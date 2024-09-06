b10004d40d2dc33f0120aa884775f293aaaabba4
Min <solunar@vip.qq.com>
2024-09-06 22:19:31 +0800

    Update

--- src/views/directive.vue
+++ src/views/directive.vue
@@ -11,12 +11,26 @@ const directives = [
 </script>
 
 <template>
-  <div v-if="!active" class="absolute top-1/2 left-1/2 ml-8 w-40 backdrop-blur-sm border-y border-neutral-500 bg-neutral-900/40">
-    <template v-for="(directive, index) in directives" :key="directive.value">
-      <div v-if="index" class="h-px bg-gradient-to-r from-white/0 via-white/50"></div>
-      <q-tr :data="[directive.label]" @click="active = directive.value" hover />
-    </template>
-  </div>
+  <div class="h-full flex justify-center items-center">
+    <div v-if="!active" class="absolute top-1/2 left-1/2 ml-8 w-40 backdrop-blur-sm border-y border-neutral-500 bg-neutral-900/40">
+      <template v-for="(directive, index) in directives" :key="directive.value">
+        <div v-if="index" class="h-px bg-gradient-to-r from-white/0 via-white/50"></div>
+        <q-tr :data="[directive.label]" @click="active = directive.value" hover />
+      </template>
+    </div>
 
-  <div @click="active = ''" class="absolute right-2 bottom-2 w-40 h-40 flex justify-center items-center bg-neutral-900/70">{{ active }}</div>
+    <div v-if="active === 'metal-detector'" class="flex space-x-4">
+      <div class="w-80 py-4 backdrop-blur-sm border border-neutral-500 bg-neutral-900/40">
+        <div class="text-center pb-1">木箱</div>
+        <q-th :data="['名称', '数量/重量']" />
+        <q-tr :data="['220mm大地女神炮', '10.00t']" hover />
+        <q-tr :data="['雷暴机关枪', '10.00t']" hover />
+      </div>
+      <div class="p-1 space-y-1 backdrop-blur-sm border border-neutral-500 bg-neutral-900/40">
+        <q-icon v-for="n in 5" :key="n" size="large" />
+      </div>
+    </div>
+
+    <div @click="active = ''" class="absolute right-2 bottom-2 w-40 h-40 flex justify-center items-center bg-neutral-900/70">{{ active }}</div>
+  </div>
 </template>
