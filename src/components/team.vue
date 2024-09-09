3bee1b178255dd8bc30d371e9db20c86653d9b5b
Min <solunar@vip.qq.com>
2024-09-09 22:10:12 +0800

    Update

--- src/components/team.vue
+++ src/components/team.vue
@@ -8,5 +8,37 @@ defineOptions({ name: 'q-team' });
       <span>999999999</span>
       <q-icon class="ml-2" />
     </div>
+
+    <div v-for="(actor, index) in ['猎人', '机械师', '士兵', '波奇']" :key="index" class="px-4 py-2 space-y-2">
+      <div class="flex justify-between items-center">
+        <span>{{ actor }}</span>
+        <span>Lv.255</span>
+      </div>
+      <div class="h-0.5 flex bg-white/30"><b class="w-1/2 bg-cyan-500"></b></div>
+      <div class="flex">
+        <div class="h-16 w-8 my-1 bg-red-500"></div>
+        <div class="flex-1 flex flex-col space-y-2">
+          <div class="flex-1 flex flex-col pl-2">
+            <div class="flex-1 flex justify-between items-center"><span class="text-neutral-400 text-xs leading-none">HP</span><span>10000/99999</span></div>
+            <div class="flex-1 flex items-center">
+              <div class="w-px h-full bg-gradient-to-b from-white/0 via-white/50"></div>
+              <div class="flex-1 flex h-2 border-y border-neutral-900/50"><b class="bg-green-700 w-1/2"></b></div>
+              <div class="w-px h-full bg-gradient-to-b from-white/0 via-white/50"></div>
+            </div>
+          </div>
+          <div class="flex-1 flex pl-2">
+            <div class="h-8 w-8 bg-red-500"></div>
+            <div class="flex-1 flex flex-col pl-2">
+              <div class="flex-1 flex justify-between items-center"><span class="text-neutral-400 text-xs leading-none">SP</span><span>10000/99999</span></div>
+              <div class="flex-1 flex items-center">
+                <div class="w-px h-full bg-gradient-to-b from-white/0 via-white/50"></div>
+                <div class="flex-1 flex h-2 border-y border-neutral-900/50"><b class="bg-yellow-700 w-1/2"></b></div>
+                <div class="w-px h-full bg-gradient-to-b from-white/0 via-white/50"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </q-panel>
 </template>
