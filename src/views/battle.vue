38456d206cec0a9a6b1d97b7bae4c309850a7252
Min <solunar@vip.qq.com>
2024-09-11 20:06:18 +0800

    Update

--- src/views/battle.vue
+++ src/views/battle.vue
@@ -1,17 +1,20 @@
 <script setup></script>
 
 <template>
-  <div class="absolute top-0 right-0 px-2 flex backdrop-blur-sm bg-neutral-900/50">
-    <div v-for="(actor, index) in ['猎人', '机械师', '士兵', '波奇']" :key="index" class="w-36 px-2 py-4 space-y-1">
-      <div class="flex justify-between items-center">
-        <span>{{ actor }}</span>
-        <span class="flex space-x-0.5"><b v-for="n in 5" :key="n" :class="{ 'box-shadow': n < 3 }" class="h-1 w-1 rounded-full bg-neutral-500"></b></span>
+  <div class="absolute top-0 right-0 flex backdrop-blur-sm bg-neutral-900/50">
+    <template v-for="(actor, index) in ['猎人', '机械师', '士兵', '波奇']" :key="index">
+      <div v-if="index" class="w-px bg-gradient-to-b from-white/50"></div>
+      <div :class="{ 'bg-gradient-to-b': index === 1 }" class="w-36 p-2 space-y-1 from-neutral-900/50">
+        <div class="flex justify-between items-center">
+          <span>{{ actor }}</span>
+          <span class="flex space-x-0.5"><b v-for="n in 5" :key="n" :class="{ 'box-shadow': n < 3 }" class="h-1 w-1 rounded-full bg-neutral-500"></b></span>
+        </div>
+        <div class="h-px bg-gradient-to-r from-white/50"></div>
+        <div class="flex justify-between items-center"><span class="text-neutral-400 text-xs leading-none">HP</span><span>10000/99999</span></div>
+        <div class="flex-1 flex h-2 border border-neutral-900/50"><b class="bg-green-700 w-1/2"></b></div>
+        <div v-if="index !== 3" class="flex-1 flex h-2 border border-neutral-900/50"><b class="bg-yellow-700 w-1/2"></b></div>
       </div>
-      <div class="h-px bg-gradient-to-r from-white/50"></div>
-      <div class="flex justify-between items-center"><span class="text-neutral-400 text-xs leading-none">HP</span><span>10000/99999</span></div>
-      <div class="flex-1 flex h-2 border border-neutral-900/50"><b class="bg-green-700 w-1/2"></b></div>
-      <div v-if="index !== 3" class="flex-1 flex h-2 border border-neutral-900/50"><b class="bg-yellow-700 w-1/2"></b></div>
-    </div>
+    </template>
   </div>
 </template>
 
