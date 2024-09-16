b03d7a5b826113b65e91d6169bd7a4f75d610142
Min <solunar@vip.qq.com>
2024-09-16 16:12:19 +0800

    Update

--- src/views/battle.vue
+++ src/views/battle.vue
@@ -20,6 +20,9 @@ const data = [
       { label: '能量胶囊', value: 32, side: 'x99', icon: 'icon' },
       { label: '碱性车蜡', value: 33, side: 'x99', icon: 'icon' },
       {},
+      {},
+      {},
+      {},
       {}
     ]
   },
@@ -35,7 +38,7 @@ const data = [
       <div :class="{ 'bg-gradient-to-b': index === 1 }" class="w-36 p-2 space-y-1 from-neutral-900/50">
         <div class="flex justify-between items-center">
           <div>{{ actor }}</div>
-          <div class="flex space-x-0.5"><b v-for="n in 5" :key="n" :class="{ 'box-shadow': n < 3 }" class="h-1 w-1 rounded-full bg-neutral-500"></b></div>
+          <div class="flex space-x-0.5"><b v-for="n in 5" :key="n" :class="{ full: n < 3 }" class="h-1 w-1 rounded-full bg-neutral-500"></b></div>
         </div>
         <div class="h-px bg-gradient-to-r from-white/50"></div>
         <div class="flex justify-between items-center"><span class="text-neutral-400 text-xs leading-none">HP</span><span>10000/99999</span></div>
@@ -50,11 +53,11 @@ const data = [
     <div class="flex-1 pr-4 flex items-center justify-end">Lorem ipsum dolor sit amet.</div>
   </div>
 
-  <q-list :data="data" class="top-1/2 left-2/3" />
+  <q-list :data="data" class="top-1/2 left-1/2" />
 </template>
 
 <style scoped>
-.box-shadow {
+.full {
   background-color: theme('colors.orange.400');
   box-shadow: 0 0 5px theme('colors.orange.400');
 }
