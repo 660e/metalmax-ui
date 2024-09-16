5649ecf204d0aa0c54368b7a133a49faad25949b
Min <solunar@vip.qq.com>
2024-09-16 16:07:29 +0800

    Update

--- src/components/list.vue
+++ src/components/list.vue
@@ -16,11 +16,11 @@ const select = value => {
   <div class="absolute z-10 top-0 left-0">
     <div v-for="(item, index) in data" :key="index" @click="select(item.value)" class="w-32">
       <div v-if="index" class="h-px bg-neutral-500"></div>
-      <div :class="{ 'opacity-50': active }" class="h-6 pl-4 flex items-center backdrop-blur-sm duration-200 bg-neutral-900/70 hover:pl-2">{{ item.label }}</div>
+      <div :class="{ 'opacity-50': active }" class="clip h-6 pl-4 flex items-center backdrop-blur-sm duration-200 bg-neutral-900/70 hover:pl-2">{{ item.label }}</div>
       <div v-if="active === item.value && item.children?.length" class="absolute z-20 top-2 left-2">
         <div v-for="(e, i) in item.children" :key="i" class="w-64">
           <div v-if="i" class="h-px bg-neutral-500"></div>
-          <div class="h-6 pl-4 pr-0.5 flex items-center space-x-2 backdrop-blur-sm duration-200 bg-neutral-900/70 hover:pl-2">
+          <div class="clip h-6 px-4 flex items-center space-x-2 backdrop-blur-sm duration-200 bg-neutral-900/70 hover:pl-2">
             <div>{{ e.label }}</div>
             <div class="flex-1"></div>
             <div>{{ e.side }}</div>
@@ -31,3 +31,9 @@ const select = value => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.clip {
+  clip-path: polygon(8px 0, 100% 0, 100% calc(100% - 8px), calc(100% - 8px) 100%, 0 100%, 0 8px);
+}
+</style>
