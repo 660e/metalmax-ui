a8bb1b3bc332641280ade448ca67b2fd45a52c80
Min <solunar@vip.qq.com>
2024-10-06 15:45:11 +0800

    Update list.vue

--- src/views/mmr/components/list.vue
+++ src/views/mmr/components/list.vue
@@ -16,17 +16,26 @@ const select = (value) => {
   <div class="absolute z-10 top-0 left-0">
     <div v-for="(item, index) in data" :key="index" @click="select(item.value)" class="w-32">
       <div v-if="index" class="h-px bg-neutral-500"></div>
-      <div :class="{ 'opacity-50': active }" class="clip h-6 pl-4 flex items-center backdrop-blur-sm duration-200 bg-neutral-900/70 hover:pl-2">
-        {{ item.label }}
+      <div class="group relative">
+        <div
+          :class="{ 'opacity-50': active }"
+          class="clip h-6 pl-4 flex items-center backdrop-blur-sm duration-200 bg-neutral-900/70 hover:bg-neutral-900"
+        >
+          {{ item.label }}
+        </div>
+        <img src="../assets/cursor.png" class="absolute top-1/2 -left-4 -translate-y-1/2 hidden group-hover:block" />
       </div>
       <div v-if="active === item.value && item.children?.length" class="absolute z-20 top-2 left-2">
         <div v-for="(e, i) in item.children" :key="i" class="w-64">
           <div v-if="i" class="h-px bg-neutral-500"></div>
-          <div class="clip h-6 px-4 flex items-center space-x-2 backdrop-blur-sm duration-200 bg-neutral-900/70 hover:pl-2">
-            <div>{{ e.label }}</div>
-            <div class="flex-1"></div>
-            <div>{{ e.side }}</div>
-            <q-icon v-if="e.icon" />
+          <div class="group relative">
+            <div class="clip h-6 px-4 flex items-center space-x-2 backdrop-blur-sm duration-200 bg-neutral-900/70 hover:bg-neutral-900">
+              <div>{{ e.label }}</div>
+              <div class="flex-1"></div>
+              <div>{{ e.side }}</div>
+              <q-icon v-if="e.icon" />
+            </div>
+            <img src="../assets/cursor.png" class="absolute top-1/2 -left-4 -translate-y-1/2 hidden group-hover:block" />
           </div>
         </div>
       </div>
