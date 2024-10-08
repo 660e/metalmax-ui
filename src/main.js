e1ddae79b9a58847f9a52a2c296de46e06800acd
Min <solunar@vip.qq.com>
2024-10-08 21:50:00 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -4,12 +4,20 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import QIcon from '@/components/icon.vue';
+import QLayout from '@/components/layout.vue';
+import QList from '@/components/list.vue';
+import QNumber from '@/components/number.vue';
+import QPanel from '@/components/panel.vue';
+import QScroll from '@/components/scroll.vue';
+import QTeam from '@/components/team.vue';
+import QTh from '@/components/th.vue';
+import QTr from '@/components/tr.vue';
+
 const app = createApp(App);
 
-const QComponents = import.meta.glob('@/components/*.vue');
-Object.values(QComponents).forEach(async (module) => {
-  const component = await module();
-  app.component(component.default.name, component.default);
+[QIcon, QLayout, QList, QNumber, QPanel, QScroll, QTeam, QTh, QTr].forEach((component) => {
+  app.component(component.name, component);
 });
 
 app.use(router);
